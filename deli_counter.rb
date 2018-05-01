5383e8b55f781d4fc1619c817c210fd9695f8fe8
Dmitri Mendis <2dmendis@gmail.com>
2018-05-01 23:09:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+def line(katz_deli)
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else 
+    new_line = []
+    count = 1
+    katz_deli.each do |i|
+      new_line << "#{count}. #{i}"
+      count += 1 
+    end 
+    new_line.join
+    puts "The line is currently: #{new_line}"
+  end 
+end
